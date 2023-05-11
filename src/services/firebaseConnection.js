f5529814d300b33c28fff71a3db99b42ca4c79ea
Ruchitinica <70488141+Ruchitinica@users.noreply.github.com>
2023-05-11 16:22:11 -0300

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,12 +3,7 @@ import 'firebase/auth';
 import 'firebase/database';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyDN7cjRQ6kxeVAEfAYW0A3V9J6qtYalgw0",
-    authDomain: "financa-e5f28.firebaseapp.com",
-    projectId: "financa-e5f28",
-    storageBucket: "financa-e5f28.appspot.com",
-    messagingSenderId: "748987637709",
-    appId: "1:748987637709:web:c58c4a69966a9853a42e84"
+    
 };
 
 if (!firebase.apps.length) {
